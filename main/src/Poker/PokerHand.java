e6ba6795e980c6a2fa1fb63e74d8815d01369852
DanielNaranjo <dnartor.98@gmail.com>
2021-01-29 11:31:19 +0000

    Added value of hands

--- main/src/Poker/PokerHand.java
+++ main/src/Poker/PokerHand.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 public class PokerHand {
 
     ArrayList<Card> cards=new ArrayList();
+    int value=1;
 
     public PokerHand(String cards) {
         String[] cardsString = cards.split(" ");
         for (String card : cardsString) {
             this.cards.add(new Card(card));
         }
+        selfCombination();
     }
 
 
@@ -24,6 +26,23 @@ public class PokerHand {
             res+= card.toString()+" ";
         }
         return cards.toString().trim();
+    }
+
+    public void selfCombination(){
+        int sameNumber=1;
+        for(int i = 0; i< cards.size();i++){
+            for(int j = i+1;j<cards.size();j++){
+                if(cards.get(i).getNumValue().equals(cards.get(j).getNumValue())){
+                    sameNumber++;
+                    break;
+                }
+            }
+        }
+        value=sameNumber;
+    }
+
+    public int getValue() {
+        return value;
     }
 
     public boolean compareWith(PokerHand handToCompare){
--- main/test/PokerHand_.java
+++ main/test/PokerHand_.java
@@ -3,20 +3,37 @@ import Poker.PokerHand;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class PokerHand_ {
 
-    PokerHand pokerHand;
-    String cartas = "[KS, 2H, 5C, JD, TD]";
+    private PokerHand pokerHand;
+    private String cartas = "[KS, 2H, 5C, JD, TD]";
+    private PokerHand pokerHandWithPair;
+    private PokerHand pokerHandWithThree;
+    private PokerHand pokerHandWithPoker;
+
+
     @Before
     public void init(){
         pokerHand = new PokerHand("KS 2H 5C JD TD");
+        pokerHandWithPair = new PokerHand("KS KH 2S 4D TD");
+        pokerHandWithThree = new PokerHand("KS KH 2S KD TD");
+        pokerHandWithPoker = new PokerHand("KS KH 2S KD KD");
+
     }
     @Test
     public void comprobeConstructor(){
         assertTrue(cartas.equals(pokerHand.toString()));
     }
 
+    @Test
+    public void comprobeValue(){
+        assertEquals(2,pokerHandWithPair.getValue());
+        assertEquals(3,pokerHandWithThree.getValue());
+        assertEquals(4,pokerHandWithPoker.getValue());
+
+    }
 
 }
